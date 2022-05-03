3f877877c2ee79cc2563d0c26eb192aaa28d94e5
Jakub Lis <vulpes2005@gmail.com>
2022-05-03 20:08:00 +0200

    add spanish - english version

--- main.py
+++ main.py
@@ -3,14 +3,37 @@ from random import randint
 
 csv = read_csv('resources/thousand_most_popular_spanish_words.csv', sep=',', encoding='utf-8').to_dict()
 
-while True:
-    random_index = randint(0, len(csv['Spanish']) - 1)
-    random_word = csv["Spanish"][random_index]
-    print(random_word)
-    answer = input("Type translation: ")
-    if answer.lower() == csv["in English"][random_index].lower():
-        print("Correct!")
-    else:
-        print("Wrong! The answer is:", csv["in English"][random_index])
+print("Do you want to write Spanish or English words?\ns - Spanish\ne - English")
 
-    print("-----------------------------------")
+choice = input("Enter your choice: ")
+
+if choice == 's':
+    while True:
+        random_index = randint(0, len(csv['Spanish']) - 1)
+        random_word = csv["in English"][random_index]
+        print(random_word)
+        answer = input("Type translation: ")
+        if answer.lower() == csv["Spanish"][random_index].lower():
+            print("Correct!")
+        else:
+            print("Wrong! The answer is:", csv["Spanish"][random_index])
+
+        print("-----------------------------------")
+
+elif choice == 'e':
+    while True:
+        random_index = randint(0, len(csv['Spanish']) - 1)
+        random_word = csv["Spanish"][random_index]
+        print(random_word)
+        answer = input("Type translation: ")
+        if answer.lower() == csv["in English"][random_index].lower():
+            print("Correct!")
+        else:
+            print("Wrong! The answer is:", csv["in English"][random_index])
+
+        print("-----------------------------------")
+
+else:
+    print("Invalid option")
+
+
